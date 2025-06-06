71cf229d615f7d2e12de837ac03c6887328107e4
peadarx1 <walshp27@tcd.ie>
2025-06-06 13:37:46 +0100

    Added toggles for the price lines

--- app.py
+++ app.py
@@ -1,4 +1,4 @@
-from dash import Dash, html, dcc
+from dash import Dash, Input, Output, callback, html, dcc
 from utils.data_loader import load_stock_data
 import plotly.express as px
 
@@ -13,8 +13,25 @@ fig = px.line(df, x = "Date", y = "Price", color="Type", title="Microsoft Daily 
 # App layout
 app.layout = [
     html.H1("MSFT Stock Dashboard"),
-    dcc.Graph(figure=fig)
+    dcc.Graph(id='graph-with-slider'),
+    dcc.Checklist(
+        options=["Open", "Close", "Low", "High"],
+        value=["Open", "Close", "Low", "High"],
+        id="line-checklist"
+    )
 ]
+
+@callback(
+    Output(component_id="graph-with-slider", component_property="figure"),
+    Input(component_id="line-checklist", component_property="value")
+)
+def update_figure(lines_selected) :
+    # print(lines_selected)
+    filtered_df = df[df['Type'].isin(lines_selected)]
+    fig = px.line(filtered_df, x='Date', y="Price", color="Type", title="MSFT Price Moves")
+    
+    return fig
+    
 
 
 # Run the app
